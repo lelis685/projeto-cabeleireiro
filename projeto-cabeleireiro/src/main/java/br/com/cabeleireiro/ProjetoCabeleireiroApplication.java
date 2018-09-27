22c20688b13ade3e162eed8cbc3c4c6c3c24394e
lelis685 <lelis685@gmail.com>
2018-09-27 14:10:21 -0300

    criação do projeto

---
+++ projeto-cabeleireiro/src/main/java/br/com/cabeleireiro/ProjetoCabeleireiroApplication.java
@@ -0,0 +1,12 @@
+package br.com.cabeleireiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCabeleireiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCabeleireiroApplication.class, args);
+	}
+}
