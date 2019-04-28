9eed2caa1f5379a2edc8b6d1564e7bd02861654c
lelis685 <36525573+lelis685@users.noreply.github.com>
2019-04-28 00:12:39 -0300

    Update ProjetoCabeleireiroApplication.java

--- projeto-cabeleireiro/src/main/java/br/com/cabeleireiro/ProjetoCabeleireiroApplication.java
+++ projeto-cabeleireiro/src/main/java/br/com/cabeleireiro/ProjetoCabeleireiroApplication.java
@@ -10,11 +10,6 @@ public class ProjetoCabeleireiroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoCabeleireiroApplication.class, args);
 	
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-	
-		System.out.println("-*************************");
-		System.err.println(bCryptPasswordEncoder.matches("130297", "$2a$10$tT6p0Q6kQ7FKi2.fkhrVCucSLxF.41zEa/HHi791l9hry6Z6WskT."));
-	
 	}
 
 	
